bb18a88d354fe1c5efdb655fac55c0864c271c13
Lechen Zhang <leczhang@umich.edu>
2024-05-07 15:14:20 +0000

    Minor edit on sample running fng script

--- sample_run.sh
+++ sample_run.sh
@@ -1,6 +1,6 @@
 ## declare an array variable
 declare -a models=("/shared/4/models/llama2/pytorch-versions/llama-2-7b-chat")
-declare -a benchmarks=("socket_bragging#brag_achievement" "socket_hahackathon#is_humor" "socket_tweet_irony")
+declare -a benchmarks=("socket_bragging#brag_achievement" "socket_hahackathon#is_humor" "socket_tweet_irony" "socket_sexyn" "socket_tweet_offensive" "socket_complaints" "socket_empathy#empathy_bin" "socket_stanfordpoliteness" "socket_rumor#rumor_bool")
 
 ## now loop through the above array
 for i in "${models[@]}"
@@ -8,6 +8,8 @@ do
     for j in "${benchmarks[@]}"
     do
         echo "Now running $j on $i"
-        CUDA_VISIBLE_DEVICES=1 python scripts/inference_vllm.py -model_dir $i -benchmark $j -system_prompts_dir "./data/system_prompts/Prompt-Scores_Baseline.csv" -saving_strategy "eval" -cache_dir "/shared/4/models/"
+        #CUDA_VISIBLE_DEVICES=1 python scripts/inference_vllm.py -model_dir $i -benchmark $j -system_prompts_dir "./data/system_prompts/Prompt-Scores_Baseline.csv" -saving_strategy "eval" -cache_dir "/shared/4/models/"
+        #CUDA_VISIBLE_DEVICES=1 python scripts/inference_vllm.py -model_dir $i -benchmark $j -system_prompts_dir "./data/system_prompts/Prompt-Scores_Good-Property.csv" -cache_dir "/shared/4/models/"
+        #CUDA_VISIBLE_DEVICES=1 python scripts/inference_vllm.py -model_dir $i -benchmark $j -system_prompts_dir "./data/system_prompts/Prompt-Scores_Good-Property.csv" -cache_dir "/shared/4/models/"
     done
 done
