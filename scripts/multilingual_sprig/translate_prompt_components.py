dbc5fc9f915fa53e8cfe561412d71ed3daa9987b
Lechen Zhang <leczhang@umich.edu>
2025-03-25 00:57:43 +0000

    Add new lang (es) to repo.

--- scripts/multilingual_sprig/translate_prompt_components.py
+++ scripts/multilingual_sprig/translate_prompt_components.py
@@ -2,16 +2,17 @@ import json
 from translate import translate_text
 from tqdm import tqdm
 
+target_lang = "es"
 
-# 读取 JSONL 文件并翻译 prompt 字段
-input_file = "../../data/system_prompts/generated_prompt_components_20250315_sup.jsonl"
-output_file = input_file.replace(".jsonl", "_zh.jsonl")
+
+input_file = "../../data/system_prompts/generated_prompt_components_20250315.jsonl"
+output_file = input_file.replace(".jsonl", f"_{target_lang}.jsonl")
 
 with open(input_file, "r", encoding="utf-8") as infile, open(output_file, "w", encoding="utf-8") as outfile:
     for line in tqdm(infile):
         data = json.loads(line.strip())  # 解析 JSONL 行
-        data["prompt"] = translate_text(data["prompt"])  # 翻译 prompt
+        data["prompt"] = translate_text(data["prompt"], target=target_lang)  # 翻译 prompt
         print(data["prompt"])
         outfile.write(json.dumps(data, ensure_ascii=False) + "\n")  # 写回 JSONL 文件
 
-print(f"翻译完成，结果已保存至 {output_file}")
+print(f"Translation done. Saved at {output_file}")
